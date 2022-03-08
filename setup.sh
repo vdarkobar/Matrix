1a4dd4a3b2efe67a5ebec4bf594b6c5921b25e9f
Darko Vukovic <vdarkobar@gmail.com>
2022-03-08 18:53:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
+sudo apt update
 
+wait -n
+
+echo "Update completed."
+
+sudo docker run -it --rm \
+   -v "$PWD/synapse:/data" \
+   -e SYNAPSE_SERVER_NAME=matrix.example.com \
+   -e SYNAPSE_REPORT_STATS=yes \
+   -e UID=1000 \
+   -e GID=1000 \
+   matrixdotorg/synapse:latest generate \
+
+wait -n
+
+echo "Job completed."
