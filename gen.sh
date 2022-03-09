60cf1686b28c3e1b45b577e565b2bd59c201db5b
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 11:51:35 +0100

    Create gen.sh

---
+++ gen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Generating a configuration file."
+sleep 1
+sudo docker run -it --rm \
+   -v "$PWD/synapse:/data" \
+   -e SYNAPSE_SERVER_NAME=matrix.example.com \
+   -e SYNAPSE_REPORT_STATS=yes \
+   -e UID=1000 \
+   -e GID=1000 \
+   matrixdotorg/synapse:latest generate
+wait #waits for all child background jobs to complete
+echo "Generating a configuration file completed."
+sleep 1
