c6031fb55b03368411b85193d40d609110728e08
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 12:06:04 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -6,9 +6,8 @@ sudo docker run -it --rm \
    -v "$PWD/synapse:/data" \
    -e SYNAPSE_SERVER_NAME=matrix.example.com \
    -e SYNAPSE_REPORT_STATS=yes \
-   -e UID=1000 \
-   -e GID=1000 \
+   -e UID=CHUID \
+   -e GID=CHGID \
    matrixdotorg/synapse:latest generate
 wait #waits for all child background jobs to complete
 echo "Generating a configuration file completed."
-sleep 1
