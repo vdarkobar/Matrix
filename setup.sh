91473f031f8cf80c8a18a18eb2a617e8f3954d42
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 13:23:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 clear
-
 echo "Enter required informations:"
 echo -ne "${RED}Enter Server Local IP: "; read MSLIP; \
 echo -ne "${RED}Enter Matrix Server Domain (example.com): "; read MSDNAME; \
 echo -ne "${RED}Enter Element-Web Client Subomain (chat): "; read EWSUBDNAME; \
 echo -ne "${RED}Enter Time Zone: "; read TZONE; \
-echo -ne "${RED}Enter UID (id $USER): "; read UID; \
-echo -ne "${RED}Enter GID (id $USER): "; read GID; \
+echo -ne "${RED}Enter UID: "; read UID; \
+echo -ne "${RED}Enter GID: "; read GID; \
 echo -ne "${RED}Enter Synapse Port Number (*:8008): "; read SYNPORT; \
 echo -ne "${RED}Enter Synapse Secure Port Number (*:443): "; read SYNSPORT; \
 echo -ne "${RED}Enter Element-Web Port Number (*:80): "; read EWPORT; \
 echo -ne "${RED}Enter Redis Port Number (*:6379): "; read REDPORT; \
 echo -ne "${RED}Enter PostgreSQL Port Number (*:5432): "; read PGPORT; \
-
 sed -i "s|CHTZ|${TZONE}|" .env && \
 sed -i "s|CHUID|${UID}|" .env && \
 sed -i "s|CHGID|${GID}|" .env && \
@@ -31,10 +29,8 @@ sed -i "s|CHMSLIP|${MSLIP}|" nginx/matrix.conf && \
 sed -i "s|CHSYN1|${SYNPORT}|" nginx/matrix.conf && \
 sed -i "s|CHDOM|${MSDNAME}|" nginx/www/.well-known/matrix/client && \
 sed -i "s|CHDOM|${MSDNAME}|" nginx/www/.well-known/matrix/server && \
-
 REDIS_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32); sed -i "s|CHPWR|${REDIS_PASSWORD}|" .env && \
 echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 63 > secrets/db_password.secret && \
-
 sudo chown -R root:root secrets/ && \
 sudo chmod -R 600 secrets/ && \
 rm README.md && \
