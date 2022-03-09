f4064871ae8fe1ec1717cbdd1b0f1d72cc18f77a
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 13:27:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,24 +5,24 @@ echo -ne "${RED}Enter Server Local IP: "; read MSLIP; \
 echo -ne "${RED}Enter Matrix Server Domain (example.com): "; read MSDNAME; \
 echo -ne "${RED}Enter Element-Web Client Subomain (chat): "; read EWSUBDNAME; \
 echo -ne "${RED}Enter Time Zone: "; read TZONE; \
-echo -ne "${RED}Enter UID: "; read UID; \
-echo -ne "${RED}Enter GID: "; read GID; \
+echo -ne "${RED}Enter UID: "; read UUID; \
+echo -ne "${RED}Enter GID: "; read GGID; \
 echo -ne "${RED}Enter Synapse Port Number (*:8008): "; read SYNPORT; \
 echo -ne "${RED}Enter Synapse Secure Port Number (*:443): "; read SYNSPORT; \
 echo -ne "${RED}Enter Element-Web Port Number (*:80): "; read EWPORT; \
 echo -ne "${RED}Enter Redis Port Number (*:6379): "; read REDPORT; \
 echo -ne "${RED}Enter PostgreSQL Port Number (*:5432): "; read PGPORT; \
 sed -i "s|CHTZ|${TZONE}|" .env && \
-sed -i "s|CHUID|${UID}|" .env && \
-sed -i "s|CHGID|${GID}|" .env && \
+sed -i "s|CHUID|${UUID}|" .env && \
+sed -i "s|CHGID|${GGID}|" .env && \
 sed -i "s|CHSYN1|${SYNPORT}|" .env && \
 sed -i "s|CHSYN2|${SYNSPORT}|" .env && \
 sed -i "s|CHEW|${EWPORT}|" .env && \
 sed -i "s|CHRP|${REDPORT}|" .env && \
 sed -i "s|CHPSQL|${PGPORT}|" .env && \
 sed -i "s|CHDOM|${MSDNAME}|" gen.sh && \
-sed -i "s|CHUID|${UID}|" gen.sh && \
-sed -i "s|CHGID|${GID}|" gen.sh && \
+sed -i "s|CHUID|${UUID}|" gen.sh && \
+sed -i "s|CHGID|${GGID}|" gen.sh && \
 sed -i "s|CHDOM|${MSDNAME}|" element-config.json && \
 sed -i "s|CHDOM|${MSDNAME}|" nginx/matrix.conf && \
 sed -i "s|CHMSLIP|${MSLIP}|" nginx/matrix.conf && \
