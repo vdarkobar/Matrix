03575883df219f304bb75ef408e0452d31c269aa
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 12:14:22 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -4,7 +4,7 @@ echo "Generating a configuration file."
 sleep 1
 sudo docker run -it --rm \
    -v "$PWD/synapse:/data" \
-   -e SYNAPSE_SERVER_NAME=matrix.example.com \
+   -e SYNAPSE_SERVER_NAME=matrix.CHDOM \
    -e SYNAPSE_REPORT_STATS=yes \
    -e UID=CHUID \
    -e GID=CHGID \
