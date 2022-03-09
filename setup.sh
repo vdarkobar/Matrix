67febdbbc9fabcd4b6110840096cbddd45c49b79
Darko Vukovic <vdarkobar@gmail.com>
2022-03-09 12:46:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,49 @@
-# Some action
+#!/bin/bash
+clear
 
-wait #waits for all child background jobs to complete
+echo "Enter required informations:"
+echo -ne "${RED}Enter Server Local IP: "; read MSLIP; \
+echo -ne "${RED}Enter Matrix Server Domain (example.com): "; read MSDNAME; \
+echo -ne "${RED}Enter Element-Web Client Subomain (chat): "; read EWSUBDNAME; \
+echo -ne "${RED}Enter Time Zone: "; read TZONE; \
+echo -ne "${RED}Enter UID (id $USER): "; read UID; \
+echo -ne "${RED}Enter GID (id $USER): "; read GID; \
+echo -ne "${RED}Enter Synapse Port Number (*:8008): "; read SYNPORT; \
+echo -ne "${RED}Enter Synapse Secure Port Number (*:443): "; read SYNSPORT; \
+echo -ne "${RED}Enter Element-Web Port Number (*:80): "; read EWPORT; \
+echo -ne "${RED}Enter Redis Port Number (*:6379): "; read REDPORT; \
 
-echo "Some action completed."
+sed -i "s|CHTZ|${TZONE}|" .env && \
+sed -i "s|CHUID|${UID}|" .env && \
+sed -i "s|CHGID|${GID}|" .env && \
+sed -i "s|CHSYN1|${SYNPORT}|" .env && \
+sed -i "s|CHSYN2|${SYNSPORT}|" .env && \
+sed -i "s|CHEW|${EWPORT}|" .env && \
+sed -i "s|CHRP|${REDPORT}|" .env && \
+sed -i "s|CHDOM|${MSDNAME}|" gen.sh && \
+sed -i "s|CHUID|${UID}|" gen.sh && \
+sed -i "s|CHGID|${GID}|" gen.sh && \
+sed -i "s|CHDOM|${MSDNAME}|" element-config.json && \
+sed -i "s|CHDOM|${MSDNAME}|" nginx/matrix.conf && \
+sed -i "s|CHMSLIP|${MSLIP}|" nginx/matrix.conf && \
+sed -i "s|CHSYN1|${SYNPORT}|" nginx/matrix.conf && \
+sed -i "s|CHDOM|${MSDNAME}|" nginx/www/.well-known/matrix/client && \
+sed -i "s|CHDOM|${MSDNAME}|" nginx/www/.well-known/matrix/server && \
 
-sudo docker run -it --rm \
-   -v "$PWD/synapse:/data" \
-   -e SYNAPSE_SERVER_NAME=matrix.example.com \
-   -e SYNAPSE_REPORT_STATS=yes \
-   -e UID=1000 \
-   -e GID=1000 \
-   matrixdotorg/synapse:latest generate
+REDIS_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32); sed -i "s|CHPWR|${REDIS_PASSWORD}|" .env && \
+echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 63 > secrets/db_password.secret && \
 
-wait #waits for all child background jobs to complete
-
-echo "Generating a configuration file completed."
+sudo chown -R root:root secrets/ && \
+sudo chmod -R 600 secrets/ && \
+rm README.md && \
+chmod +x gen.sh && \
+./gen.sh && \
+wait
+while true; do
+    read -p "Execute 'docker-compose up -d' now? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo docker-compose up -d; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
